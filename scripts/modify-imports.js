b11192698e2b2c9811b9021650cd755bba839a19
Liran <>
2024-09-02 15:07:44 +0300

    fix ERR_MODULE_NOT_FOUND error on logger

--- scripts/modify-imports.js
+++ scripts/modify-imports.js
@@ -1,6 +1,5 @@
 import { fileURLToPath } from 'url';
-import fs from "fs";
-
+import fs from 'fs';
 import { dirname } from 'path';
 import path from 'path';
 
@@ -10,12 +9,14 @@ const distDir = path.join(__dirname, '../dist');
 
 function modifyImports(filePath) {
   let content = fs.readFileSync(filePath, 'utf8');
-  content = content.replace(/from\s+["'](\.\/[a-zA-Z0-9_]+)(?!\.js)["']/g, 'from "$1.js"');
+
+  content = content.replace(/from\s+['"](\.\/[^'".]+)(?!\.js)['"]/g, 'from "$1.js"');
+
   fs.writeFileSync(filePath, content, 'utf8');
 }
 
 function processDirectory(dir) {
-  fs.readdirSync(dir).forEach(file => {
+  fs.readdirSync(dir).forEach((file) => {
     const fullPath = path.join(dir, file);
     if (fs.statSync(fullPath).isDirectory()) {
       processDirectory(fullPath);
