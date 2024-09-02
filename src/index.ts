a42767fbe2f2dde603ba118d086840414e98d360
Liran <>
2024-09-02 19:42:33 +0300

    Added more detailed logging on app start

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,47 @@
-import 'dotenv/config';
+import "dotenv/config";
 import express from "express";
 import { CronJob } from "cron";
 import fs from "fs";
 import { filterEpg } from "./filterEpg";
 import path from "path";
-import { logger, LogLevel } from './lib/logger';
-
+import { logger, LogLevel } from "./lib/logger";
+import packageJson from "../package.json";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-const filteredEpgFilePath = path.join(process.cwd(),process.env.EPG_OUTPUT_FILE_PATH ?? 'epg.xml');
+const filteredEpgFilePath = path.join(
+  process.cwd(),
+  process.env.EPG_OUTPUT_FILE_PATH ?? "epg.xml"
+);
 
 const filterEPG = () => {
-  logger.log(LogLevel.INFO,"Filtering EPG ...");
+  logger.log(LogLevel.INFO, "Filtering EPG ...");
   filterEpg();
 };
 // Cron job to run the filter function periodically
-const job = new CronJob(`0 */${process.env.FETCH_HOUR_INTERVAL ?? 12} * * *`, () => {
-  filterEPG();
-});
+const job = new CronJob(
+  `0 */${process.env.FETCH_HOUR_INTERVAL ?? 12} * * *`,
+  () => {
+    filterEPG();
+  }
+);
 
 job.start();
-if(!(process.env.FETCH_ON_START?.toLowerCase() === "false")) filterEPG();
+if (!(process.env.FETCH_ON_START?.toLowerCase() === "false")) filterEPG();
 
 app.get(process.env.EPG_OUTPUT_API_RESULT_PATH ?? "/epg.xml", (req, res) => {
   // Read the filtered EPG file
-  fs.readFile(filteredEpgFilePath, 'utf-8', (err, data) => {
+  fs.readFile(filteredEpgFilePath, "utf-8", (err, data) => {
     if (err) {
       // Handle error, e.g., file not found or read error
-      logger.log(LogLevel.ERROR, 'Error reading file:', err);
-      res.status(500).send('Internal Server Error');
+      logger.log(LogLevel.ERROR, "Error reading file:", err);
+      res.status(500).send("Internal Server Error");
       return;
     }
 
     // Set the appropriate Content-Type header for XML
-    res.setHeader('Content-Type', 'application/xml');
+    res.setHeader("Content-Type", "application/xml");
 
     // Send the file contents as a response
     res.send(data);
@@ -54,5 +60,26 @@ app.get("/crontime", (req, res) => {
 });
 
 app.listen(port, () => {
-  logger.log(LogLevel.INFO,`Server running at http://localhost:${port}. Fetching EPG every ${process.env.FETCH_HOUR_INTERVAL ?? 12} hours. Source EPG URL path: ${process.env.SOURCE_EPG_URL_PATH ?? "UNSET"}. Source EPG File path: ${process.env.SOURCE_EPG_LOCAL_FILE ?? "UNSET"}. Using local EPG File: ${(process.env.SOURCE_USE_LOCAL_FILE?.toLowerCase() === "true") ? "YES" : "NO"}.`);
+  logger.log(LogLevel.INFO, `Server running at http://localhost:${port} ...`);
 });
+
+
+logger.log(LogLevel.INFO, `App Version: ${packageJson.version}`);
+logger.log(
+  LogLevel.INFO,
+  `Fetching EPG every ${process.env.FETCH_HOUR_INTERVAL ?? 12} hours ...`
+);
+logger.log(
+  LogLevel.INFO,
+  `Source EPG URL path: ${process.env.SOURCE_EPG_URL_PATH ?? "UNSET"}`
+);
+logger.log(
+  LogLevel.INFO,
+  `Source EPG File path: ${process.env.SOURCE_EPG_LOCAL_FILE ?? "UNSET"}`
+);
+logger.log(
+  LogLevel.INFO,
+  `Using local EPG File: ${
+    process.env.SOURCE_USE_LOCAL_FILE?.toLowerCase() === "true" ? "YES" : "NO"
+  }`
+);
